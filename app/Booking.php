b83079c0587f027813b33007b665c82136e14835
Unknown <zsoltmaschek@gmail.com>
2018-12-08 16:29:23 +0100

    Update booking model

--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $table = "bookings";
+    public $primaryKey = "id";
+    public $incrementing = true;
+    public $timestamps = false;
+    protected $fillable = ['id', 'id_user', 'id_room', 'id_meal', 'date_from', 'date_to'];
+
+    public function user() {
+        return $this->hasOne('App\User', 'id', 'id_user');
+    }
+
+    public function room() {
+        return $this->hasOne('App\Room', 'id', 'id_room');
+    }
+
+    public function meal() {
+        return $this->hasOne('App\Meal', 'id', 'id_meal');
+    }
 }
